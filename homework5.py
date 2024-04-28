dcd77314a6cfca292474e6670798fecfc1767719
arturlarcev2001 <45388557+arturlarcev2001@users.noreply.github.com>
2024-04-28 21:18:40 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['apple', 'banana', 'peach']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'strawberry'
+print(my_list)
+
+my_dict = {'Enfant' : 'Ребенок', 'Garcon' : 'Мальчик', 'Fille' : 'Девочка'}
+print(my_dict)
+print(my_dict['Enfant'])
+my_dict.update(Du_pain = 'Хлеб')
+print(my_dict)
