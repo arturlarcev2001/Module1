6883117519d5387853e2ed44279e4c141445b0fe
arturlarcev2001 <45388557+arturlarcev2001@users.noreply.github.com>
2024-05-23 11:31:37 +0500

    Update homework7.py

--- homework7.py
+++ homework7.py
@@ -1,6 +1,14 @@
-cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
-cars_count = 0
+my_dict = {'Andrew' : 2002, 'Arthur' : 2001, 'Maxim' : 2003}
+print(my_dict)
+print(my_dict['Andrew'])
+my_dict['John'] = 1958
+my_dict['Bred'] = 2072
+del my_dict['Andrew']
+print(my_dict)
 
-for i in cars:
-    print('Я езжу на автомабиле марки', i)
-    cars_count += 10
+my_set = {1, 1, 2, 3, '1', True}
+print(my_set)
+my_set.add(5)
+my_set.add('George Washington')
+my_set.discard(1)
+print(my_set)
