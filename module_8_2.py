359219402e01e46f870c0359a0831d62ff7a21ff
arturlarcev2001 <45388557+arturlarcev2001@users.noreply.github.com>
2024-06-29 16:13:34 +0500

    Add files via upload

---
+++ module_8_2.py
@@ -0,0 +1,33 @@
+
+
+class Integer(Exception):
+    def __init__(self, message, extra_message):
+        self.message = message
+        self.extra_message = extra_message
+
+class String(Exception):
+    def __init__(self, message, extra_message):
+        self.message = message
+        self.extra_message = extra_message
+
+
+def generate_raise(a):
+    if isinstance(a, int):
+        raise Integer(f"Использован целий тип аргумента", {"a": a})
+    elif isinstance(a, str):
+        raise String(f"Использован строковый тип аргумента", {"a": a})
+    else:
+        print("Нет исключений")
+
+
+def try_func(func, value):
+    try:
+        func(value)
+    except (Integer, String) as e:
+        print(e.message)
+        print(e.extra_message)
+        print("Конец исключения")
+    finally:
+        print("---")
+
+try_func(generate_raise, [])
