d9d677b5a05eef96a38ca3171c24937682cbf498
arturlarcev2001 <45388557+arturlarcev2001@users.noreply.github.com>
2024-07-17 22:06:43 +0500

    Add files via upload

--- module_6_2.py
+++ module_6_2.py
@@ -1,19 +1,51 @@
 
 
 class Vehicle:
-    vehicle_type = "none"
 
-class Car:
-    price = 1000000
-    def horse_powers(self):
-        return self.powers
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+    
+    def __init__(self, owner, model, engine_power, color):
+        self.owner = owner
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = color
+    
+    def get_model(self):
+        return "Модель: {}".format(self.__model)
+    def get_horsepower(self):
+        return "Мощность двигателя: {}".format(self.__engine_power)
+    def get_color(self):
+        return "Цвет: {}".format(self.__color)
 
-class Nissan(Car, Vehicle):
-    vehicle_type = "sedan"
-    price = 2000000
-    def horse_powers(self, powers):
-        return powers
+    def print_info(self):
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print("Владелец: {}".format(self.owner))
+    def set_color(self, new_color):
+        for color in self.__COLOR_VARIANTS:
+            if color.lower() == new_color.lower():
+                self.__color = new_color
+                return
 
-car = Nissan
-print(car.vehicle_type)
-print(car.price)
+class Sedan(Vehicle):
+
+    __PASSANGER_LIMIT = 5
+    
+    def __init__(self, owner, model, engine_power, color):
+        super().__init__(owner, model, engine_power, color)
+
+
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+# # Изначальные свойства
+vehicle1.print_info()
+
+# # Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+# # Проверяем что поменялось
+vehicle1.print_info()
+
