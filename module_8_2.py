1ed2c5e615c0fb7d4ce7c6e02d73d5603b5cf263
arturlarcev2001 <45388557+arturlarcev2001@users.noreply.github.com>
2024-07-19 10:06:33 +0500

    Add files via upload

--- module_8_2.py
+++ module_8_2.py
@@ -1,33 +1,34 @@
 
 
-class Integer(Exception):
-    def __init__(self, message, extra_message):
-        self.message = message
-        self.extra_message = extra_message
+def personal_sum(numbers):
+    sums = 0
+    incorrect_data = 0
+    for n in numbers:
+        try:
+            sums += n
+        except TypeError:
+            incorrect_data += 1
+    return (sums, incorrect_data)
+    
 
-class String(Exception):
-    def __init__(self, message, extra_message):
-        self.message = message
-        self.extra_message = extra_message
-
-
-def generate_raise(a):
-    if isinstance(a, int):
-        raise Integer(f"Использован целый тип аргумента", {"a": a})
-    elif isinstance(a, str):
-        raise String(f"Использован строковый тип аргумента", {"a": a})
-    else:
-        print("Нет исключений")
-
-
-def try_func(func, value):
+def calculate_average(numbers):
     try:
-        func(value)
-    except (Integer, String) as e:
-        print(e.message)
-        print(e.extra_message)
-        print("Конец исключения")
-    finally:
-        print("---")
+        try:
+            sums = personal_sum(numbers)
+            if isinstance(sums, tuple):
+                aver = sums[0] / (len(numbers)-sums[1])
+                return aver
+            else:
+                aver = sums / len(numbers)
+                return aver
+        except ZeroDivisionError:
+            return 0
+    except:
+            return None
 
-try_func(generate_raise, [])
+    
+print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
+
