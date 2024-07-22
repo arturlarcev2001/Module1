fc94bbe7a3fa010ff8c1809fc16e7a8b70b6d19d
arturlarcev2001 <45388557+arturlarcev2001@users.noreply.github.com>
2024-07-23 00:37:36 +0500

    Add files via upload

--- module_9_7.py
+++ module_9_7.py
@@ -8,13 +8,17 @@ def is_prime(func):
             if n % i == 0:
                 l.append(i)
         if len(l) > 2:
-            return "Составное"
+            print("Составное")
+            return n
         else:
-            return "Простое"
+            print("Простое")
+            return n
     return wrapper
 
 @is_prime
 def sum_three(a, b, c):
     return a + b + c
 
+
 print(sum_three(2, 3, 6))
+
