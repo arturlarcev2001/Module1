b2c2e29a0178bf7f384281bd27ae16e4d3ae786b
arturlarcev2001 <45388557+arturlarcev2001@users.noreply.github.com>
2024-07-27 18:14:42 +0500

    Update module_10_4.py

--- module_10_4.py
+++ module_10_4.py
@@ -2,13 +2,13 @@ from threading import Thread
 from time import sleep
 from queue import Queue
 
-# Класс для столов
+
 class Table:
     def __init__(self, number):
         self.number = number
         self.is_busy = False
 
-# Класс для симуляции работы кафе
+
 class Cafe:
     def __init__(self, tables):
         self.queue = Queue()
@@ -19,9 +19,9 @@ class Cafe:
         while customer_number <= 20:
             print(f"Посетитель номер {customer_number} прибыл.")
             customer_thread = Customer(customer_number, self)
-            customer_thread.start()  # Используем start() для начала потока
+            customer_thread.start()  
             customer_number += 1
-            sleep(1)  # Искусственная задержка между приходами посетителей
+            sleep(1)  
 
     def serve_customer(self, customer):
         table_found = False
@@ -29,8 +29,8 @@ class Cafe:
             if not table.is_busy:
                 table.is_busy = True
                 print(f"Посетитель номер {customer.number} сел за стол {table.number}.")
-                sleep(5)  # Время обслуживания 5 секунд
-                table.is_busy = False  # Освободили столик после обслуживания
+                sleep(5)  
+                table.is_busy = False  
                 print(f"Посетитель номер {customer.number} покушал и ушёл.")
                 table_found = True
                 break
@@ -39,7 +39,7 @@ class Cafe:
             self.queue.put(customer)
             self.queue.get()
 
-# Класс для посетителей
+
 class Customer(Thread):
     def __init__(self, number, cafe):
         super().__init__()
@@ -49,18 +49,17 @@ class Customer(Thread):
     def run(self):
         self.cafe.serve_customer(self)
 
-# Создаем столы в кафе
+
 table1 = Table(1)
 table2 = Table(2)
 table3 = Table(3)
 tables = [table1, table2, table3]
 
-# Инициализируем кафе
+
 cafe = Cafe(tables)
 
-# Запускаем поток для прибытия посетителей
+
 customer_arrival_thread = Thread(target=cafe.customer_arrival)
 customer_arrival_thread.start()
 
-# Ожидаем завершения работы прибытия посетителей
 customer_arrival_thread.join()
